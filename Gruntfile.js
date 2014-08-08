584c06e9956de4fdec5f4b91063ab61302792d2d
Stephan Bönnemann <stephan@boennemann.me>
2014-08-08 17:54:52 +0200

    chore(grunt): simplify build

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,28 +9,14 @@ module.exports = function(grunt) {
 
     clean: [
       '<%= config.dist %>/hoodie.css',
-      '<%= config.dist %>/hoodie.scss',
-      '<%= config.dist %>/hoodie-dev.css',
-      '<%= config.dist %>/hoodie-dev.scss',
-      'bower_components/hoodie-base-css'
+      '<%= config.dist %>/hoodie.css.map',
     ],
-
-    replace: {
-      all: {
-        src: ['src/*.scss'], // source files array (supports minimatch)
-        dest: 'bower_components/hoodie-base-css/', // destination directory or file
-        replacements: [{
-          from: '$BOWER-COMPONENT-PATH', // string replacement
-          to: '..'
-        }]
-      }
-    },
 
     sass: { // task
       dev: { // target
         files: [{ // dictionary of files
           dest: '<%= config.dist %>/hoodie-dev.css',
-          src: 'bower_components/hoodie-base-css/hoodie.scss'
+          src: '<%= config.src %>/hoodie.scss'
         }]
       },
       dist: { // another target
@@ -39,32 +25,8 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         },
         files: [{
-          dest: 'bower_components/hoodie-base-css/hoodie.css',
-          src:  'bower_components/hoodie-base-css/hoodie.scss'
-        }]
-      }
-    },
-
-    copy: {
-      dev: {
-        files: [{
-          src: 'bower_components/hoodie-base-css/hoodie.scss',
-          dest: '<%= config.dist %>/hoodie-dev.scss'
-        }, {
-          src: 'bower_components/hoodie-base-css/hoodie-dev.css',
-          dest: '<%= config.dist %>/hoodie-dev.css'
-        }]
-      },
-      dist: {
-        files: [{
-          src: 'bower_components/hoodie-base-css/hoodie.scss',
-          dest: '<%= config.dist %>/hoodie.scss'
-        }, {
-          src: 'bower_components/hoodie-base-css/hoodie.css',
-          dest: '<%= config.dist %>/hoodie.css'
-        }, {
-          src: 'bower_components/hoodie-base-css/hoodie.css.map',
-          dest: '<%= config.dist %>/hoodie.css.map'
+          dest: '<%= config.dist %>/hoodie.css',
+          src: '<%= config.src %>/hoodie.scss'
         }]
       }
     }
@@ -77,15 +39,11 @@ module.exports = function(grunt) {
 
   grunt.registerTask('dev', [
     'clean',
-    'replace',
-    'sass:dev',
-    'copy:dev'
+    'sass:dev'
   ]);
 
   grunt.registerTask('dist', [
     'clean',
-    'replace',
-    'sass:dist',
-    'copy:dist'
+    'sass:dist'
   ]);
 };
